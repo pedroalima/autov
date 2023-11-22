14d17a7243dc35f94b99dfa93b1471734b863e16
Pedro Lima <tsc.pedrolima@gmail.com>
2023-11-21 21:52:22 -0300

    feat: add image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,54 +1,54 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
-import * as M from "@mui/material"
-import { Menu } from '@mui/icons-material';
-import Drawer from './drawer';
+import { useState } from "react";
+import * as M from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import Drawer from "./drawer";
 
 const navItems = ["Home"];
 
 export default function Navbar() {
-    const [mobileOpen, setMobileOpen] = useState(false);
+	const [mobileOpen, setMobileOpen] = useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
-    };
+	const handleDrawerToggle = () => {
+		setMobileOpen((prevState) => !prevState);
+	};
 
-    return (
-        <>
-            <M.AppBar component="nav">
-                <M.Toolbar>
-                    <M.Typography
-                        variant="h6"
-                        component="div"
-                        textTransform="uppercase"
-                        sx={{ flexGrow: 1 }}
-                    >
+	return (
+		<>
+			<M.AppBar component="nav">
+				<M.Toolbar>
+					<M.Typography
+						variant="h6"
+						component="div"
+						textTransform="uppercase"
+						sx={{ flexGrow: 1 }}
+					>
                         Auto V
-                    </M.Typography>
+					</M.Typography>
 
-                    <M.IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
-                    >
-                        <Menu />
-                    </M.IconButton>
+					<M.IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={handleDrawerToggle}
+						sx={{ mr: 2, display: { sm: "none" } }}
+					>
+						<Menu />
+					</M.IconButton>
                     
-                    <M.Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        {navItems.map((item) => (
-                        <M.Button key={item} sx={{ color: '#fff' }}>
-                            {item}
-                        </M.Button>
-                        ))}
-                    </M.Box>
-                </M.Toolbar>
-            </M.AppBar>
-            <nav>
-                <Drawer navItems={navItems} mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
-            </nav>
-        </>
-    )
+					<M.Box sx={{ display: { xs: "none", sm: "block" } }}>
+						{navItems.map((item) => (
+							<M.Button key={item} sx={{ color: "#fff" }}>
+								{item}
+							</M.Button>
+						))}
+					</M.Box>
+				</M.Toolbar>
+			</M.AppBar>
+			<nav>
+				<Drawer navItems={navItems} mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
+			</nav>
+		</>
+	);
 }
