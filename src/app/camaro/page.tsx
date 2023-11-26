dcc312d77d4e75b3d318884eaac5f07796ef0d54
Pedro Lima <tsc.pedrolima@gmail.com>
2023-11-25 22:18:34 -0300

    feat: add data in components

--- src/app/camaro/page.tsx
+++ src/app/camaro/page.tsx
@@ -1,3 +1,4 @@
+import { data } from "@/utils/data";
 import * as M from "@mui/material";
 import Image from "next/image";
 
@@ -32,16 +33,19 @@ export default function Camaro() {
 					gap="2rem"
 				>
 					<M.Box textAlign="center">
-						<M.Typography component="h3">Corvertt</M.Typography>
+						<M.Typography component="h3">Camaro</M.Typography>
 						<M.Typography component="p">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</M.Typography>
 						<M.Typography component="a">Site</M.Typography>
 					</M.Box>
 
 					<M.Card sx={{ width: "90%"}}>
-						<M.CardContent sx={{ display: "flex", justifyContent: "space-between"}}>
-							<M.Typography component="h6">Exemplo</M.Typography>
-							<M.Typography component="span">135415 km</M.Typography>
-						</M.CardContent>
+						{data && data.map(veiculo => (
+							<M.CardContent key={veiculo.camaro?.name} sx={{ display: "flex", justifyContent: "space-between"}}>
+								<M.Typography component="h6">Velocidade Máxima</M.Typography>
+								<M.Typography component="span">{veiculo.camaro?.maxSpeed} km</M.Typography>
+							</M.CardContent>
+						))}
+						
 					</M.Card>
 				</M.Grid>
 			</M.Grid>
